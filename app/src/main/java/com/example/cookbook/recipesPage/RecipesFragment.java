9b4538057891a67498b74aaef35f2b0428ac04ac
Johanne Boisvilliers <johanne.boisvilliers@gmail.com>
2019-10-14 18:05:12 +0200

    add staggered recyclerview for recipes

--- app/src/main/java/com/example/cookbook/recipesPage/RecipesFragment.java
+++ app/src/main/java/com/example/cookbook/recipesPage/RecipesFragment.java
@@ -7,14 +7,23 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import com.example.cookbook.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class RecipesFragment extends Fragment {
 
+    @BindView(R.id.all_recipes_recyclerview)
+    RecyclerView mRecipesRecyclerView;
+
+    private RecipesListAdapter mListAdapter;
 
     public RecipesFragment() {
         // Required empty public constructor
@@ -27,8 +36,20 @@ public class RecipesFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recipes, container, false);
+        View view = inflater.inflate(R.layout.fragment_recipes, container, false);
+        ButterKnife.bind(this, view);
+
+        this.configureRecyclerview();
+
+        return view;
+    }
+
+    private void configureRecyclerview() {
+        mListAdapter = new RecipesListAdapter();
+        StaggeredGridLayoutManager layoutManager = new StaggeredGridLayoutManager(2,
+                StaggeredGridLayoutManager.VERTICAL);
+        mRecipesRecyclerView.setAdapter(mListAdapter);
+        mRecipesRecyclerView.setLayoutManager(layoutManager);
     }
 
 }
