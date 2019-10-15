437de5e0d13d824ad0e8d9ddd3c293699547ca86
Johanne Boisvilliers <johanne.boisvilliers@gmail.com>
2019-10-15 11:38:30 +0200

    add default user's profile photo

--- app/src/main/java/com/example/cookbook/profilePage/ProfileFragment.java
+++ app/src/main/java/com/example/cookbook/profilePage/ProfileFragment.java
@@ -5,16 +5,23 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.fragment.app.Fragment;
 
+import com.bumptech.glide.Glide;
 import com.example.cookbook.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ProfileFragment extends Fragment {
 
+    @BindView(R.id.background_user_profile)
+    ImageView mUserProfilePhoto;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -29,7 +36,20 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        ButterKnife.bind(this, view);
+
+        this.configureBackground();
+
+        return view;
+    }
+
+    // ------------------------------------ UI ------------------------------------
+    private void configureBackground() {
+        Glide.with(this)
+                .load(R.drawable.no_photo)
+                .fitCenter()
+                .into(mUserProfilePhoto);
     }
 
 }
