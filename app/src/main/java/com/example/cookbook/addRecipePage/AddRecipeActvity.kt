9bb0a2e153715dbc25d72c69f019deda6ee0999b
Johanne Boisvilliers <johanne.boisvilliers@gmail.com>
2020-02-29 17:27:37 +0100

    if user check the 'recipe online' box, a field for url appear

--- app/src/main/java/com/example/cookbook/addRecipePage/AddRecipeActvity.kt
+++ app/src/main/java/com/example/cookbook/addRecipePage/AddRecipeActvity.kt
@@ -2,13 +2,29 @@ package com.example.cookbook.addRecipePage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.CheckBox
 import com.example.cookbook.R
+import kotlinx.android.synthetic.main.activity_add_recipe.*
 
 class AddRecipeActvity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_recipe)
+
+    }
+
+    fun onCheckBoxClicked(view: View){
+        if(view is CheckBox){
+            val checked: Boolean = view.isChecked
+            if(checked){
+                recipe_url.visibility = View.VISIBLE
+            }else{
+                recipe_url.visibility = View.GONE
+            }
+        }
+
 
     }
 }
